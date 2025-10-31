e4ec621b7029cb8d8cca97a46d704926bd1ed6e6
Neyolim <neyoollim@gmail.com>
2025-10-31 15:51:42 +0545

    add Reverse String problem

---
+++ ReverseString.js
@@ -0,0 +1,36 @@
+// Problem statement:
+// - Write a function that returns the reverse of a string
+// - Input : A string
+// - Return : reversed string
+// - Example = "Neyong" -> "gnoyeN"
+
+// Method - 1
+
+function reverseString(string) {
+  if (typeof string !== "string") {
+    throw new TypeError("Only String is allowed !!!");
+  }
+  reverse = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverse += string[i];
+  }
+  return reverse;
+}
+
+console.log(reverseString("Ram"));
+console.log(reverseString("Neyong"));
+console.log(reverseString("Software Development"));
+console.log(reverseString(23424)); // breaks
+
+// // Method - 2
+
+// function reverseString2(string) {
+//     let result = string.split('').reverse().join('');
+//     console.log(result)
+// }
+
+// reverseString2("Ram")
+// reverseString2("Neyong")
+// reverseString2("Software Development")
+
+// Logic is important, but thorough testing across diverse scenarios is what ensures your code remains robust and reliable.
