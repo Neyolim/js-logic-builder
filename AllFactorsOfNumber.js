a239f2a457a6404edb55b2e3d911a490a46906d9
Neyool <neyoollim@gmail.com>
2025-11-15 15:27:23 +0000

    demo PaaS

--- AllFactorsOfNumber.js
+++ AllFactorsOfNumber.js
@@ -4,6 +4,6 @@
 // - Return: Factors of a number
 // - Factor: Is a number that divides the given number evenly or exactly, leaving no remainder
 // - Example:
-//     - 4: 1,2,4
-//     - 14: 1,2,7,14
-//     - 35: 1,5,,7,35
+//     - 4: 1, 2, 4
+//     - 14: 1, 2, 7, 14
+//     - 35: 1, 5, 7, 35
