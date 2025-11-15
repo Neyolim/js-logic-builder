5ea2ece231048ae09cb66960a90196c62fc18e86
Neyolim <neyoollim@gmail.com>
2025-11-15 20:31:34 +0545

    add AllFactorsOfNumber.js problem

---
+++ AllFactorsOfNumber.js
@@ -0,0 +1,9 @@
+// Problem statement:
+// - Write a function that finds all the factors of a given number.
+// - Input: number, positive integer
+// - Return: Factors of a number
+// - Factor: Is a number that divides the given number evenly or exactly, leaving no remainder
+// - Example:
+//     - 4: 1,2,4
+//     - 14: 1,2,7,14
+//     - 35: 1,5,,7,35
