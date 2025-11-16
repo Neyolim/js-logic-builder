c562a2ff627d7cd2d3309226a5351cc77d8a391c
Neyolim <neyoollim@gmail.com>
2025-11-16 18:15:34 +0545

    solve AllFactorsOfNumber.js problem

--- AllFactorsOfNumber.js
+++ AllFactorsOfNumber.js
@@ -6,4 +6,24 @@
 // - Example:
 //     - 4: 1,2,4
 //     - 14: 1,2,7,14
-//     - 35: 1,5,,7,35
+//     - 35: 1,5,7,35
+
+function factorsOfNumber(num) {
+  if (typeof num !== "number" || isNaN(num)) {
+    throw new Error("Input must be a valid number!");
+  }
+  if (num < 1 || !Number.isInteger(num)) {
+    throw new Error("Input number should be a positive integer!");
+  }
+  let factors = [];
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      factors.push(i);
+    }
+  }
+  return factors;
+}
+
+console.log(factorsOfNumber(4));
+console.log(factorsOfNumber(14));
+console.log(factorsOfNumber(35));
