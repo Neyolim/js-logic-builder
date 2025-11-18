e348e78eee6f21af321c1279d7bed021fc355c47
Neyolim <neyoollim@gmail.com>
2025-11-18 15:34:30 +0545

    add CelsiusToFahrenheitConverter.js  problem to implement temperature conversion from Celsius to Fahrenheit

---
+++ CelsiusToFahrenheitConverter.js
@@ -0,0 +1,9 @@
+// Problem statement:
+// - Write a function that converts a temperature in Celsius to Fahrenheit.
+// - Input: Temperature in Celsius
+// - Return: Temperature in Fahrenheit
+//     - Celsius & Fahrenheit are 2 different scales two measure temperature
+//     - Formula (Temperature in degrees Celsius(C) * 9/5) + 32
+//     - Example:
+//         - 27 degrees = (27 * 9/5) + 32 = 80.6 Fahrenheit
+//         - 34 degrees = (34 * 9/5) + 32 = 93.2 Fahrenheit
