a9bdf657f5c9b671ce9bd69c71c173695f69274b
Neyolim <neyoollim@gmail.com>
2025-11-20 17:37:36 +0545

    solve CelsiusToFahrenheitConverter.js problem

--- CelsiusToFahrenheitConverter.js
+++ CelsiusToFahrenheitConverter.js
@@ -7,3 +7,16 @@
 //     - Example:
 //         - 27 degrees = (27 * 9/5) + 32 = 80.6 Fahrenheit
 //         - 34 degrees = (34 * 9/5) + 32 = 93.2 Fahrenheit
+
+function tempConversion(temp) {
+  if (typeof temp !== "number") {
+    throw new Error("Input should be number !!1");
+  }
+  let result = (temp * 9) / 5 + 32;
+  result = Math.round(result);
+  return result;
+}
+
+console.log(tempConversion(27));
+console.log(tempConversion(34));
+console.log(tempConversion(-34));
