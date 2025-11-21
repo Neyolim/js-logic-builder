7d8b804d4b9cc722408b33b6f5a3562da76f89bc
Neyolim <neyoollim@gmail.com>
2025-11-21 10:15:42 +0545

    add simple interest problem

---
+++ SimpleInterest.js
@@ -0,0 +1,12 @@
+// Problem statement:
+// - Write a function that calculates and prints the simple interest on a loan amount.
+// - Input:
+//     - Principal amount
+//     - Interest rate (yearly)
+//     - Time duration (how many years)
+// - Return: Interest amount
+// - Simple Interest:
+//     - (Principal * Interest Rate * Time Duration) / 100
+// - Example:
+//     - 1000, 5%, 1 year = (1000 * 5 * 1) / 100 = 50
+//     - 20000, 10%, 5 = (20000 * 10 * 5) / 100 = 10000
