2b3945e758a25578236517e11227f37df56eb630
Neyolim <neyoollim@gmail.com>
2025-11-22 21:18:44 +0545

    add CheckingPrimeNumber.js problem

---
+++ CheckingPrimeNumbers.js
@@ -0,0 +1,12 @@
+// Problem statement:
+// - Write a function that tells if the given number is prime or not.
+// - Input: A number
+// - Return: true if prime and false if not prime
+// - Prime number: A number that can be only be divided by itself and 1 without remainders
+// - Example: 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
+// - Example of 5:
+//     - 1 - 5/1 -> 0
+//     - 2 - 5/2 -> 1
+//     - 3 - 5/3 -> 2
+//     - 4 - 5/4 -> 1
+//     - 5 - 5/5 -> 0
