af5384af33aa1fb7ed334b7d73773286d47b677f
Neyolim <neyoollim@gmail.com>
2025-11-24 22:14:06 +0545

    add SwappingNumbers.js problem

---
+++ SwappingNumbers.js
@@ -0,0 +1,8 @@
+// Problem Statement: 
+// - Write a program that swap two numbers 
+// - Input: Two numbers a & b 
+// - Output: Interchange the values of a & b 
+// - Example: 
+//     - Input: a = 5, b = 6
+//     - Output: a = 6, b = 5
+
