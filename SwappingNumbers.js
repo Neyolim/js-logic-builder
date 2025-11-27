7565cdb2a7fcd1a9d14341eec661f36e6109163f
Neyolim <neyoollim@gmail.com>
2025-11-27 08:19:53 +0545

    solve  SwappingNumbers.js

--- SwappingNumbers.js
+++ SwappingNumbers.js
@@ -1,8 +1,28 @@
-// Problem Statement: 
-// - Write a program that swap two numbers 
-// - Input: Two numbers a & b 
-// - Output: Interchange the values of a & b 
-// - Example: 
+// Problem Statement:
+// - Write a program that swap two numbers
+// - Input: Two numbers a & b
+// - Output: Interchange the values of a & b
+// - Example:
 //     - Input: a = 5, b = 6
 //     - Output: a = 6, b = 5
+
+/* Method- 1 */
+
+let a = 5;
+let b = 6;
+let c = a + b; // c = 11
+a = c - a;     // a = 11 - 5 = 6
+b = c - b;     // b = 11 - 6 = 5
+console.log(a, b); // Output: 6, 5
+
+
+
+/* Method - 2 */
+
+let a = 5;
+let b = 6;
+let c = b;  // c = 6
+b = a;      // b = 5
+a = c;      // a = 6
+console.log(a, b); // Output: 6, 5
 
