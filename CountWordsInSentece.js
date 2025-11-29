0e64923e9ad8324ec9fdb44d53d79f09023c7fc9
Neyolim <neyoollim@gmail.com>
2025-11-29 22:15:32 +0545

    add CountWordsInSentence.js problem statement

---
+++ CountWordsInSentece.js
@@ -0,0 +1,7 @@
+// Problem Statement:
+// - Write a function that counts and returns the number of words in a sentence. 
+// - Input: A sentence 
+// - Return: number of words
+// - Example: 
+//     - "This is a sentence" : 4 
+//     - "What is the date today" : 5
