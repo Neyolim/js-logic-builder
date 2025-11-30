59bb0c9fb02d392242196a18441b4969f2c22318
Neyolim <neyoollim@gmail.com>
2025-11-30 19:18:11 +0545

    add ArmstrongNumberChecker.js problem statement

---
+++ ArmstrongNumberChecker.js
@@ -0,0 +1,10 @@
+// Problem statement:
+// - Write a program that checks if a number is Armstrong or not
+// - Input : A number
+// - Output: True if a number is Armstrong and False if number is not a Armstrong
+// - Armstrong: Its a number that is equal to the sum of its own digits raised to the power of the number of digits
+// - Example
+//     - 153 = (1^3) + (5^3) + (3^3) = 1 + 125 + 27 = 153 = true
+//     - 370 = (3^3) + (7^3) + (0^3) = 27 + 343 + 0 = 370 = true
+//     - 243 = (2^3) + (4^3) + (3^3) = 8 + 64 + 27 = 99 = false
+// - Few more examples: 153,370,371,407,1634,8208,9474,54748,92727
