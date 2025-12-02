0ad9220a3744d24396e0890dbef91a51afb629b4
Neyolim <neyoollim@gmail.com>
2025-12-02 10:45:00 +0545

    add NumberPyramid.js problem statement

---
+++ NumberPyramid.js
@@ -0,0 +1,10 @@
+// Problem statement:
+// - Write a program that prints a number pyramid
+// - Input: A positive number
+// - Output: Number Pyramid
+// - Example - 5
+//         1
+//        121
+//       12321
+//      1234321
+//     123454321
