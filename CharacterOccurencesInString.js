24f18a431889ac0ec48c1f8afb7de111e337475e
Neyolim <neyoollim@gmail.com>
2025-12-03 21:32:34 +0545

    add CharacterOccurencesInString.js problem statement

---
+++ CharacterOccurencesInString.js
@@ -0,0 +1,14 @@
+// Problem statement:
+// - Write a program that counts the occurences of each character in a given string
+// - Input: A string
+// - Output: Count of each character in the string
+// - Example
+//     - "Ashish Saluja"
+//         - a: 3,
+//         - s: 3,
+//         - h: 2,
+//         - i: 1,
+//         - '': 1,
+//         - l: 1,
+//         - u: 1,
+//         - j: 1
