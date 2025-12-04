7e2f364b37d094e46e30f594729e5bf5797f12f2
Neyolim <neyoollim@gmail.com>
2025-12-04 18:50:45 +0545

    add CheckingAlphabeticalOrderInStrings.js problem statement

---
+++ CheckingAlphabeticalOrderInStrings.js
@@ -0,0 +1,9 @@
+// Problem statement:
+// - Write a program that checks if a given string is in alphabetical order
+// - Input: A string
+// - Output:
+//     - true if all characters are in alphabetical order
+//     - false if characters are NOT in alphabetical order
+// - Example
+//     - "acehkosvz": true
+//     - "abcab": false
