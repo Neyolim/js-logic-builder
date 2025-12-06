1f749c6cb2c1cee5accd2c8931de3408ff5cdfb9
Neyolim <neyoollim@gmail.com>
2025-12-06 21:24:23 +0545

    add CheckAnagrams.js problem statement

---
+++ CheckAnagrams.js
@@ -0,0 +1,13 @@
+// Problem statement:
+// - Write a program that checks if two given strings are anagrams of each other
+// - Input: Two strings
+// - Output:
+//     - true if two string are anagrams
+//     - false if two string are not anagrams
+// - Anagram is a word that's formed by rearranging the letters of another word
+// - Examples
+//     - cat, act
+//     - elbow, below
+//     - night, thing
+//     - heart, earth
+//     - pools, spool
